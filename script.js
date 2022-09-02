cef032b71709c766b06e42cf39b15f7429162e84
awchen <awchen85@gmail.com>
2022-09-01 21:49:49 -0400

    added array for tasks. set up data bank, working on todays date

--- script.js
+++ script.js
@@ -1,6 +1,72 @@
+let tasks = JSON.parse(localStorage.getItem('tasks'));
+
+if (!tasks) {
+
+  tasks = [
+    {
+      time: 8,
+      tasks: [""]
+    },
+    {
+      time: 9,
+      tasks: [""]
+    },
+    {
+      time: 10,
+      tasks: [""]
+    },
+    {
+      time: 11,
+      tasks: [""]
+    },
+    {
+      time: 12,
+      tasks: [""]
+    },
+    {
+      time: 13,
+      tasks: [""]
+    },
+    {
+      time: 14,
+      tasks: [""]
+    },
+    {
+      time: 15,
+      tasks: [""]
+    },
+    {
+      time: 16,
+      tasks: [""]
+    },
+    {
+      time: 17,
+      tasks: [""]
+    },
+  ];
+}
+
+var currentDay = document.getElementById("currentDay");
+var currentTime = document.getElementById("hour");
+var timeBlock = document.getElementById("time-slot");
+var taskList = document.getElementById("task-text");
+var dateTime = function () {
+  var todaysDate = moment();
+  currentDay.textContent = todaysDate.format("LLLL");
+}
+
+$(document).ready(function () {
+  dateTime();
+  setInteravl(dateTime, 1000)
+});
+
+
+
+
 $( "#save-btn" ).click(function() {
 console.log ("click!");
 });
+
 
 function myFunction() {
   var x = document.createElement("BUTTON");
